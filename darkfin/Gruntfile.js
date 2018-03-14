7585d035edd70222e43bbc739f713bd95cd3435a
Barron Chugg <bchugg@Happy-Fun-iMac.local>
2018-03-13 19:53:49 -0700

    update

--- darkfin/Gruntfile.js
+++ darkfin/Gruntfile.js
@@ -1,94 +1,122 @@
 
 module.exports = function (grunt) {
-  var header = `<html><head><style>
-  .stage { background-color: #ddd; margin: 10px 30px; }
-  .actor { background-color: #cee; }
-  .line { background-color: #eec; }
-  .actor:after { content: ":"; }
-  </style>
+  var _ = require('lodash');
+  var header = `<!DOCTYPE html> 
+  <html>
+  <head>
+    <title>Darkfin</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="df-favicon.png" type="image/x-icon">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   </head>
   <body>
+    <div class="wrapper">
   `;
 
-  // ../files/y3c22.html
+  var yearTitle = {
+    1: 'Duquesa Bay',
+    2: 'Chaparral Heights',
+    3: 'Quartz Lake',
+    4: 'Santa Conchita'
+  }
+  var storyFilePath = '/files/';
 
-  var footer = `</body></html>`;
+  var resourcePath = '../working-files/resources/';
+  var storyPath = '../working-files/story/';
+  var baseHtml = grunt.file.read(resourcePath + 'template.html');
+  var cssFileName = 'main.css';
+  var outPath = 'src/files/';
+  var h2regex = /<h2>([\s\S]*?)<\/h2>/gm;
+
+  var timestamp = (new Date()).getTime();
+  var footer = `</div></body></html>`;
   var imagePath = '../assets/images/';
   var allFiles = [];
   grunt.initConfig({
     post: {
       run: {}
     },
-    copy: {
+    watch: {
+      files: ['../working-files/**/*.*'],
+      tasks: ['sass:main', 'post:run']
+    },
+    sass: {
       main: {
-        src: '../files/*.html',
-        dest: './src/out/',
-        options: {
-          process: function (content, srcpath) {
-            var fileName = srcpath.split('/files/')[1].split('.html')[0], year, chapter, num, title = '', foot = '', nextFile, prevFile;
-            var h2 = /<h2>\s*?(.*?)\s*?<\/h2>/gm.exec(content);
-            if (fileName.indexOf('y') === 0 && fileName.indexOf('c') === 2) {
-              year = +fileName[1];
-              chapter = +fileName.substr(3, 2);
-              title = '<h1>Year ' + year + ', Chapter ' + chapter + '</h1>';
-              nextFile = 'y' + year + 'c' + (chapter + 1 < 10 ? '0' + (chapter + 1) : chapter + 1) + '.html';
-              prevFile = 'y' + year + 'c' + (chapter - 1 < 10 ? '0' + (chapter - 1) : chapter - 1) + '.html';
-              if (grunt.file.isFile('../files/' + prevFile)) {
-                foot += '<div>prev file = ' + prevFile + '</div>';
-              }
-              if (grunt.file.isFile('../files/' + nextFile)) {
-                foot += '<div>next file = ' + nextFile + '</div>';
-              }
-              allFiles.push({
-                year: year,
-                chapter: chapter,
-                fileName: fileName + '.html',
-                title: h2 && h2[1] ? h2[1].replace(/&quot;/gm, '') : ''
-              });
-            } else if (fileName.indexOf('v') === 0) {
-              chapter = +fileName.substr(1, 2);
-              title = '<h1>Vignette ' + chapter + '</h1>';
-              nextFile = 'v' + (chapter + 1 < 10 ? '0' + (chapter + 1) : chapter + 1) + '.html';
-              prevFile = 'v' + (chapter - 1 < 10 ? '0' + (chapter - 1) : chapter - 1) + '.html';
-              if (grunt.file.isFile('../files/' + prevFile)) {
-                foot += '<div>prev file = ' + prevFile + '</div>';
-              }
-              if (grunt.file.isFile('../files/' + nextFile)) {
-                foot += '<div>next file = ' + nextFile + '</div>';
-              }
-              allFiles.push({
-                year: 'v',
-                chapter: chapter,
-                fileName: fileName + '.html',
-                title: h2 && h2[1] ? h2[1].replace(/&quot;/gm, '') : ''
-              });
-            }
-            console.log(fileName, h2 ? h2[1] : '--');
-
-            return header +
-              title +
-              content
-                .replace(/<stage>/g, '<div class="stage">')
-                .replace(/<\/stage>/g, '</div>')
-                .replace(/<line>/g, '<p class="line">')
-                .replace(/<\/line>/g, '</p>')
-                .replace(/<actor.*?>/g, '<span class="actor">')
-                .replace(/<\/actor>/g, '</span>')
-                .replace(/\.\.\/graphics\//g, imagePath) +
-              foot +
-              footer;
-          },
-        },
+        files: {
+          'src/files/main.css': resourcePath + 'main.scss'
+        }
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 
-  grunt.registerTask('default', ['copy:main', 'post:run']);
+  grunt.registerTask('makecss', ['sass:main']);
+  grunt.registerTask('default', ['post:run']);
 
   grunt.task.registerMultiTask('post', 'post', () => {
-    // console.log(allFiles);
+    let structure = [];
+    let files = grunt.file.expand(storyPath + '*.html');
+
+    files.forEach(path => {
+      let fileOut = baseHtml;
+      let fileInfo = {};
+      let file = grunt.file.read(path);
+      let h = (/<h2>([\s\S]*?)<\/h2>/gm).exec(file);
+      fileInfo.fileName = path.replace(storyPath, '');
+      fileInfo.title = h ? h[1] : 'Not Found';
+      fileInfo.type = fileInfo.fileName[0]; // y or v
+      fileInfo.year = fileInfo.type === 'y' ? fileInfo.fileName[1] : null;
+      fileInfo.yearTitle = fileInfo.type === 'y' ? yearTitle[fileInfo.year] : null;
+      fileInfo.chapter = fileInfo.type === 'y' ? +fileInfo.fileName.substr(3, 2) : fileInfo.type === 'v' ? +fileInfo.fileName.substr(1, 2) : null;
+
+      let nextFile = fileInfo.type +
+        (fileInfo.year ? (fileInfo.year) : '') +
+        (fileInfo.type === 'y' ? 'c' : '') +
+        ('0' + (fileInfo.chapter + 1)).slice(-2) + '.html';
+
+      let prevFile = fileInfo.type +
+        (fileInfo.year ? (fileInfo.year) : '') +
+        (fileInfo.type === 'y' ? 'c' : '') +
+        ('0' + (fileInfo.chapter - 1)).slice(-2) + '.html';
+
+      fileInfo.prevFile = grunt.file.isFile(storyPath + prevFile) && prevFile;
+      fileInfo.nextFile = grunt.file.isFile(storyPath + nextFile) && nextFile;
+      structure.push(fileInfo);
+
+      file = file
+        .replace(/<h2>([\s\S]*?)<\/h2>/, '')
+        .replace(/<bar><\/bar>/g, '<hr>')
+        .replace(/<stage>/g, '<div class="stage">')
+        .replace(/<\/stage>/g, '</div>')
+        .replace(/<line>/g, '<p class="line">')
+        .replace(/<\/line>/g, '</p>')
+        .replace(/<actor.*?>/g, '<span class="actor">')
+        .replace(/<\/actor>/g, '</span>')
+        .replace(/\.\.\/graphics\//g, imagePath);
+
+
+      fileOut = fileOut
+        .replace(/%%time%%/g, timestamp)
+        .replace(/%%title%%/g, fileInfo.title)
+        .replace(/%%chapter%%/g, fileInfo.type === 'y' ? `Year ${fileInfo.year}, Chapter ${fileInfo.chapter}` : fileInfo.type === 'v' ? `Vignette ${fileInfo.chapter}` : '')
+        .replace(/%%body%%/g, file)
+        .replace(/%%prev%%/g, fileInfo.prevFile ? `<a href="${fileInfo.prevFile}"><i class="fa fa-chevron-left"></i>Prev</a>` : '')
+        .replace(/%%next%%/g, fileInfo.nextFile ? `<a href="${fileInfo.nextFile}">Next<i class="fa fa-chevron-right"></i></a>` : '');
+
+      grunt.file.write(outPath + fileInfo.fileName, fileOut);
+
+    });
+
+
+    // grunt.file.write(outPath + cssFileName, grunt.file.read(resourcePath + cssFileName));
+    // grunt.file.write(outPath + 'story-main.css', grunt.file.read(resourcePath + 'story-main.css'));
+
+    grunt.file.write(outPath + 'story.html', grunt.file.read(resourcePath + 'story.html'));
+    grunt.file.write(outPath + 'story.json', JSON.stringify(structure));
+
   });
 
 };
